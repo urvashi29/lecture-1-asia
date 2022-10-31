dff104b796ca10efd81a41a5fb8d7daf504edd5e
urvashi29 <urvashigolu29@gmail.com>
2022-10-31 14:11:40 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// App: class based component
+class App extends Component {
+  //js object
+  state = {
+    name: 'alina',
+    age: 20,
+    designation: 'developer',
+    arrOfObjects: [{}, {}, {}]
+  }
+
+
+  handleClick = () => {
+    //to update a state
+    this.setState({
+      age: 25
+    });
+  }
+
+  //
+  render() {
+    return (
+      //jsx-> js syntx extension
+
+      //this is referring to App
+      <div>
+        <h2>Hello</h2>
+        <p>Hi</p>
+        My name is {this.state.name} and age is {this.state.age}, working as a {this.state.designation}.
+        <button onClick={this.handleClick}>Click</button>
+      </div>
+    )
+  }
 }
 
+
 export default App;
+
+{/* <button onclick="">Click</button> */ }
+
+// Diffing algo
+// reconcilation process
+
+//inheritance
+
+
+//js objects
+// var obj = {
+//   name: 'alina',
+//   age: 20,
+//   designation: 'developer',
+//   details: function () {
+
+//   }
+// }
+
+
+// ES6 Features:
+// let, const, arrow function, default function arguments, class, inheritance, spread operator, rest operator, array helper function(map, filter, find, forEach, some, every, reduce)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var person = {
+//   name: 'alina'
+// }
+
+
+// function add() {
+
+// }
